aa16553c68226efe0681a9ac8c7586821a95801c
Atsushi Hashimoto <ahasimoto@mm.media.kyoto-u.ac.jp>
2016-10-02 21:16:29 +0900

    add basic files for pip install and so on.

---
+++ isoclique/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from .isolated_cliques import find_isolated_cliques
+ 
+__author__  = 'AtsushiHashimoto'
+__version__ = '0.0.1'
+__license__ = 'MIT'
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+from isoclique import __author__, __version__, __license__
+ 
+setup(
+        name             = 'isoclique',
+        version          = __version__,
+        description      = 'Implementation of Isolated Clique Enumeration Algorithm proposed in "Linear-Time Enumeration of Isolated Cliques". See http://link.springer.com/chapter/10.1007%2F11561071_13 for algorithm details.',
+        license          = __license__,
+        author           = __author__,
+        author_email     = 'ahasimoto@mm.media.kyoto-u.ac.jp',
+        url              = 'https://github.com/AtsushiHashimoto/isolated_clique_enumeration.git',
+        keywords         = 'isolated cliques',
+        packages         = find_packages(),
+        install_requires = [numpy],
+        )
