38fceb4585032b06f298dbacc2719e341d7fa6fd
Atsushi Hashimoto <ahasimoto@mm.media.kyoto-u.ac.jp>
2016-10-14 13:56:51 +0900

    implementation done before evaluation stage in enumerate_gen.

--- isoclique/isolated_cliques.py
+++ isoclique/isolated_cliques.py
@@ -3,10 +3,12 @@
 
 import numpy as np
 import math
-
+from warnings import warn
+import itertools as iters
 
 class IsolatedCliques():
-    def __init__(self, edge_list,node_range='auto'):
+    def __init__(self, edge_list,node_range='auto',debug_mode=True):
+        self.debug_mode = debug_mode
         if node_range != 'auto':
             self.node_range = node_range
         else:
@@ -21,26 +23,42 @@ class IsolatedCliques():
         degrees, self.lut, self.lut_inv = self._sort_nodes(edge_list)
         self.pivot_entries = list(zip(degrees, self._get_neighbors(edge_list,degrees)))
 
+        if self.debug_mode and not self._debug_check_order(range(self.n_nodes)):
+            warn("DEBUG: graph is not correctly sorted")
+        if self.debug_mode and not self._debug_check_edge_symmetry(range(self.n_nodes)):
+            warn("DEBUG: graph edges are not in symmetry.")
+            
+
+            
+
         #for idx,(deg,neigh) in enumerate(self.pivot_entries):
         #    print("orig_id:",self.lut[idx]," degree: ", deg, " neighbors:", [self.lut[idx] for idx in neigh])
 
+    def evaluate_subgraph(self,V,assume_clique=False):
+        if assume_clique:
+            return self._evaluate_subgraph_assume_clique(V), None, None
+        return self._evaluate_subgraph(V)
+            
     def enumerate(self,isolation_factor=1.0, callback=None):
         if isolation_factor==1.0:
             ics = self._enumerate1()
         else:
             c_floor = my_floor(isolation_factor)
             ics = self._enumerate_gen(isolation_factor,c_floor)
-       
+
+        return ics
+        '''
         iso_cliques = []
         for ic in ics:
             iso_cliques.append([self.lut[i] for i in ic])
         return iso_cliques
+        '''
     
     def _enumerate1(self):
         # case: isolation_factor=1
         pivots_idx = []
         pivots = []
-        for idx, pivot_entry in enumerate(self.pivot_entries):
+        for idx, pivot_entry in enumerate(self.pivot_entries):            
             if not self._one_pivot_test_a(idx,*pivot_entry,pivots_idx):
                 continue
             if not self._one_pivot_test_b(idx,*pivot_entry):
@@ -59,7 +77,7 @@ class IsolatedCliques():
             C = self._pivot_trim(idx,*pivot_entry,c, c_floor)
             if not C:               
                 continue
-            C = self._pivot_trim(idx,*pivot_entry,C,c,c_floor)
+            C = self._pivot_enum(idx,*pivot_entry,C,c,c_floor)
             pivots_idx.append(idx)
             pivots.append(C)
 
@@ -70,8 +88,24 @@ class IsolatedCliques():
     '''
     def _pivot_enum(self, idx, deg, neigh,C,c,c_floor):
         neigh_ = [n for n in neigh if n<idx]
-        c_dd = c_floor - (len(C)-len(neigh))
-        
+        c_dd = c_floor - (len(neigh)-len(C))
+        C_comp,C_is_clique = self.complement(C)
+        if C_is_clique:
+            Qs = [C]
+        else:
+            Qs_comp = vertex_cover(C,C_comp, at_most = c_dd, V_search=C.copy().remove(idx))
+            Qs = [[v for v in C if v not in q] for q in Qs_comp]
+        return Qs
+
+    def complement(self, C):
+        nodes = [[] for c in C]
+        is_clique = True
+        for i,j in iters.combinations(C,2):
+            if j not in self.pivot_entries[i][1]:
+                nodes[i].append(j)
+                nodes[j].append(i)
+                is_clique = False
+        return nodes,is_clique
         
     def _pivot_trim(self, idx, deg, neigh, c, c_floor):
         neigh_ = [n for n in neigh if n<idx]
@@ -192,6 +226,55 @@ class IsolatedCliques():
         neighbors = [sorted(neigh,key=lambda x:degrees[x]) for neigh in neighbors]        
         return neighbors
 
+    def evaluate_subgraph_assume_clique(self,V):
+        k = len(V)
+        n_out_edges = 0
+        for v in V:
+            n_out_edges += self.pivot_entries[v][0]-(k-1)
+        return n_out_edges/k
+
+    def evaluate_subgraph(self,V):
+        if self.debug_mode and not self._debug_check_order(V,False):
+            warn("DEBUG: Clique V is not sorted.")
+#            warn("V => ["+" ".join(map(str,V)) + "]")
+            
+        k = len(V)
+        min_deg = k
+        ave_deg = 0
+        n_out_edges = 0
+        for v in V:
+            deg, neigh = self.pivot_entries[v]
+            inner_neigh = comm_seq(V,neigh)
+            n_out_edges += len(neigh)-len(inner_neigh)            
+            min_deg = min(min_deg,len(inner_neigh))
+            ave_deg += len(inner_neigh)
+        print(k)
+        return n_out_edges/k, ave_deg/k, min_deg
+
+    def _debug_check_order(self,V,deep=False):
+        if not is_sorted([self.pivot_entries[x][0] for x in V]):
+            warn("in _debug_check_order(self,V): pivots V is not sorted in their degree.")
+            return False
+        if not deep:
+            return True
+        
+        for v in V:
+            if not is_sorted([self.pivot_entries[x][0] for x in self.pivot_entries[v][1]]):
+                warn("in _debug_check_order(self,V): neighbors of %d is not sorted in their degree."%v)
+                return False
+        return True
+    def _debug_check_edge_symmetry(self,V):
+        for v in V:
+            for w in self.pivot_entries[v][1]:
+                if v not in self.pivot_entries[w][1]:
+                    return False
+        return True
+            
+def is_sorted(l):
+    if len(l)<2:
+        return True
+    return all(l[i] <= l[i+1] for i in range(len(l)-1))
+
 def comm_seq(arr_1, arr_2):
     if len(arr_1) == 0 or len(arr_2) == 0:
         return []
@@ -221,3 +304,19 @@ def remove_check(removed,v,c_floor):
         return False,removed
     return True,removed
 
+def vertex_cover(V,G, at_most=-1,V_search=None):
+    VCs = []
+    if not V_search:
+        V_search = V
+    if at_most<0:
+        at_most = len(V_search)
+
+    for k in range(at_most,0,-1):
+        for vc_cand in iters.combinations(V_search,k):
+            covered_nodes = set(vc_cand+sum([G[x] for x in vc_cand]))
+            if len(covered_nodes)<len(V):
+                continue
+            VCs.append(vc_cand)
+
+    return VCs
+        
